f49668fe32664d7d032e3fa233265acd4b94a56f
Yin Li <strayinsky@gmail.com>
2013-09-02 23:52:53 -0700

    added a few things to the connect view

--- friends/views.py
+++ friends/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, request
 from django.views.generic.base import View, TemplateResponseMixin, TemplateView
+from friends.models import Activity
 
 
 class HomeView(TemplateView):
@@ -9,7 +10,12 @@ class HomeView(TemplateView):
 class ConnectView(TemplateView):
     template_name = 'friends/connect.html'
 
-    def context_data(self, **kwargs):
-        context = super(Connectview, self).get_context_data(**kwargs)
-        context['user'] = request.user
+    def get_context_data(self, **kwargs):
+        print "i got here!!"
+        context = super(ConnectView, self).get_context_data(**kwargs)
+        #context['user'] = request.user
+        context['activities'] = Activity.objects.all
+        for act in Activity.objects.all():
+            print act.name
+        context['times'] = {"Friday", "Saturday", "Sunday", "Monday"}
         return context
